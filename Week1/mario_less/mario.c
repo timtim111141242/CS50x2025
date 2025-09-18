1d8c1cf775dff887260d41db9ce788637126d649
Timzzzz <143259880+timtim111141242@users.noreply.github.com>
2025-09-19 01:35:51 +0800

    Create mario.c

---
+++ Week1/mario_less/mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int space, int length);
+int main(void)
+{
+    int height = get_int("Height: ");
+    if (height <= 0)
+    {
+        height = get_int("Height: ");
+    }
+
+    for (int j = 0; j < height; j++)
+    {
+        print_row(height - j - 1, j + 1);
+    }
+}
+
+void print_row(int space, int length)
+{
+    for (int i = 0; i < space; i++)
+    {
+        printf(" ");
+    }
+    for (int i = 0; i < length; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
